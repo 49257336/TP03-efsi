fd4be9094482a50383296b1d3850fb1c913ecc01
MILENA WINER <49257336@ort>
2026-04-01 08:59:27 -0300

    Cambios en el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import Listado from './Listado.jsx'
 
 function App() {
   return (
-    <>
-      <h1>ADMINISTRADOR DE PACIENTES</h1>
-      <div className="container">
-        <div className="row">
-          <Formulario />
-          <Listado />
+      <>
+        <h1>ADMINISTRADOR DE PACIENTES</h1>
+        <div className="container">
+          <div className="row">
+            <Formulario />
+            <Listado />
+          </div>
         </div>
-      </div>
-    </>
-
+      </>
   );
 }
 
