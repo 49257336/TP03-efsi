aac3565fd8902d0792ace696c75e0fe01b02ab98
MILENA WINER <49257336@ort>
2026-04-01 08:29:59 -0300

    Se crearon todos los componentes

---
+++ src/Cita.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function Cita(Mascota, Dueño, Fecha, Hora, Sintomas){
+    return(
+        <div className="cita">
+              <p>Mascota: <span>{Mascota}</span></p>
+              <p>Dueño: <span>{Dueño}</span></p>
+              <p>Fecha: <span>{Fecha}</span></p>
+              <p>Hora: <span>{Hora}</span></p>
+              <p>Sintomas: <span>{Sintomas}</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
+            </div>
+    )
+}
+
+export default Cita
---
+++ src/Listado.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './Cita.jsx'
+
+function Listado(){
+    return(
+        <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            <Cita Mascota="Nina" Dueño="Martin" Fecha="2021-08-05" Hora="08:20" Sintomas="Le duele la pierna"/>
+            <Cita Mascota="Sifon" Dueño="Flecha" Fecha="2023-08-05" Hora="09:24" Sintomas="Duerme mucho"/>
+            <Cita Mascota="Floki" Dueño="Ari" Fecha="2023-08-05" Hora="16:15" Sintomas="No está comiendo"/>
+            </div>
+    );
+}
+
+export default Listado
