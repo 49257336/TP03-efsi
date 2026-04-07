0e00a7ac799d5220364434dc381c5cb55f70a259
MILENA WINER <49257336@ort>
2026-04-07 15:28:01 -0300

    Inicio tp-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import Formulario from './Formulario.jsx'
 import Listado from './Listado.jsx'
 
 
 function App() {
+  const [citas, setCitas] = useState([]);
   return (
       <>
         <h1>ADMINISTRADOR DE PACIENTES</h1>
         <div className="container">
           <div className="row">
-            <Formulario />
-            <Listado />
+            <Formulario setCitas={setCitas}/>
+            <Listado citas={citas} setCitas={setCitas}/>
           </div>
         </div>
       </>
